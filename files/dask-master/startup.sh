d5fdf8f3328c9872dd8e444e90e880a2177daebe
Vasily Vasinov <des.elyon@gmail.com>
2020-10-18 15:17:21 -0600

    Add stastup scripts

--- files/dask-master/startup.sh
+++ files/dask-master/startup.sh
@@ -2,3 +2,8 @@
 
 set -e
 
+dask-scheduler \
+  --host {{ engines.dask-master.vars.private_ip }} \
+  --port {{ engines.dask-master.vars.port }} \
+  --dashboard \
+  --dashboard-address :{{ engines.dask-master.vars.dashboard_port }}
--- files/dask-worker/startup.sh
+++ files/dask-worker/startup.sh
@@ -2,3 +2,9 @@
 
 set -e
 
+dask-worker \
+  --worker-port {{ engines.dask-worker.vars.port }} \
+  --nanny-port {{ engines.dask-worker.vars.nanny_port }} \
+  --dashboard \
+  --dashboard-address :{{ engines.dask-worker.vars.dashboard_port }} \
+  --nthreads {{ engines.dask-worker.deployment.hardware.cpu.vcpus }}
